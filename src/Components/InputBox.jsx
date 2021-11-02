4252b1aff4f34bec7c07752d2dc7ef8d5d88d5fa
Prabhat <singhprabhatp.s.224@gamil.com>
2021-11-02 23:17:16 +0530

    Adding keycode-js in package file

--- src/Components/InputBox.jsx
+++ src/Components/InputBox.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+const KeyCode = require('keycode-js');
+class InputBox extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            value: this.props.value || ""
+        }
+    }
 
-const InputBox = () =>{
-    return(
-        <input type="text" className="form-control add-todo" placeholder="Add New"/>
-    )
+    handleChange(e) {
+        this.setState({value: e.target.value})
+    }
+    
+    handleKeyUp(e){
+        if(e.keyCode === KeyCode.KEY_RETURN){
+            // console.log(e)
+        }
+    }
+
+    render(){
+        return(
+            <input type="text" 
+            className="form-control add-todo"
+            onKeyUp={this.handleKeyUp.bind(this)}
+            onChange={this.handleChange.bind(this)}
+            placeholder="Add New"/>
+        )
+    }
+    
 }
 
 export default InputBox
